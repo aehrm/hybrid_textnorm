1d9c723ba648119becaed98618f5aff2e51ac93e
Anton Ehrmanntraut <anton.ehrmanntraut@uni-wuerzburg.de>
2024-06-19 16:21:37 +0200

    Baseline Norma: add contemporary dictionary to start the rule/distance-based components

--- baselines/norma/run_train_and_eval.sh
+++ baselines/norma/run_train_and_eval.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+
+norma_lexicon -w wordlist-german.txt -a words.fsm -l words.sym -c
 normalize -c norma.cfg -f /dataset/train.norma.parallel -s -t --saveonexit
 normalize -c norma.cfg -f /dataset/test.norma.orig -s | tee /proc/self/fd/2 | cut -d$'\t' -f1 > /output/test.norma.pred
